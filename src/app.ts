f41968454d4621ca582513356cf281f6e5de8668
ijazhossain <ijaz.hossain35@gmail.com>
2025-12-09 14:53:03 +0600

    tables created to DB

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
+import initDB from "./config/db";
 const app = express();
 app.use(express.json());
-
+initDB();
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
     success:true,
